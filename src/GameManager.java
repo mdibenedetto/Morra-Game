76ac067f380021b3d85696f4c15946297fa89ebb
michele dibenedetto <michele.dibe78@gmail.com>
2020-03-24 08:24:57 +0000

    documentation update

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,3 +1,55 @@
+/*
+    << Program Description >>
+    Morra Odds and Evens Variation
+    
+    This variation of the game is a two-player game, where one player is going to be the “Odds”
+    player and the other player is the “Evens” player. In each round of the game, the players will
+    simultaneously hold out between 1 and 10 fingers. 
+    
+    The winner of the round is decided based on the sum of fingers shown by both players, 
+    namely if the sum is an even number then the “Evens” player wins, otherwise 
+    if the sum is an odd number then the “Odds” player wins. 
+    
+    The winner of the round receives three points. In addition, the player whose number of 
+    fingers is closer to the sum, receives two extra points.
+
+    The winner of the game is the first player who accumulates 12 points. 
+*/
+
+/*
+    << Requirements >>
+
+    Develop an application to allow a user to play repeatedly the game “Morra Odds and Evens”
+    with a computer.  
+
+    1. At the beginning of each game the user will be prompted to choose whether
+    he/she would like to be either the “Odds” or the “Even” player. 
+    
+    2. In each round of the game, the user must decide the number of fingers to show (i.e. between 1 and 10). 
+    2.1 Similarly, in each round of the game the computer will randomly pick one number between 1 and 10. 
+    
+    3. In each round, the game displays the computer’s choice. 
+    
+    4. After each round the game displays the number of points each player has, 
+    and whether the user or the computer won the round.
+
+    5. A game finishes when one of the players accumulates 12 points. 
+    
+    6. At the end of a game, the game displays who the winner is, 
+    and a history of the numbers of fingers shown by both the
+    user and the computer per round.
+
+    7. Once a game has finished the application asks the player if he/she would like to play another
+    game. 
+    
+    8. At the end of all games, display a history of games played. 
+    
+    9. The history shows, for each game, the number of rounds won and lost by the human player, 
+    and how many even and odd numbers have been chosen by each player, and the extra points 
+    received by each player per game. 
+    All the history elements of the game should be coded using arrays. 
+*/
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -32,6 +84,7 @@ public class GameManager {
         }
         // END WHILE
 
+        // At the end of all games, display a history of games played.
         displayGameHistory();
     }
 
--- src/GameResult.java
+++ src/GameResult.java
@@ -1,3 +1,9 @@
+/**
+    The history shows, for each game:
+    - the number of rounds won and lost by the human player
+    - how many even and odd numbers have been chosen by each player
+    - the extra points received by each player per game.  
+*/
 // rami
 public class GameResult {
     int wonRounds;
