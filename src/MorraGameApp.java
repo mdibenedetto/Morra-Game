195a9e9452443861ad003a6264f32bd2935eb81b
michele dibenedetto <michele.dibe78@gmail.com>
2020-04-07 21:57:26 +0100

    code refactoring

--- src/MorraGameApp.java
+++ src/MorraGameApp.java
@@ -7,14 +7,8 @@
 public class MorraGameApp {
 
     public static void main(String[] args) {
-        clearScreen();
         System.out.println("MorraGameApp");
         GameManager gm = new GameManager();
         gm.startup();
-    }
-
-    public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
     }
 }
